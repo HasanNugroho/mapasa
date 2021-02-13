f4ff854322c0a37e5104ca89fbe5ae367245dd53
hasan <burhannur19@gmail.com>
2021-02-13 11:37:59 +0700

    revisi 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -148,6 +148,9 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+<link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
 <script>
     $('.btn-see').on('click', function () {
