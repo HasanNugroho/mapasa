2302c5672e485dd5499cb2369a7bd1fc0ef794af
hasan <burhannur19@gmail.com>
2021-02-26 18:10:52 +0700

    update ulang setelah error

--- resources/views/admin/galeri.blade.php
+++ resources/views/admin/galeri.blade.php
@@ -144,6 +144,8 @@ use Illuminate\Support\Arr;
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
 <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">{{-- preview img --}}
+<script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+
 <script>
     $(function () {
         // Multiple images preview with JavaScript
